8650bd502f0e3d24b0c0f40d383f82a60ce58b01
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-08-22 17:34:25 +0530

    first commit

---
+++ android/app/src/main/java/wrteam/eshop/multivendor/eshop_multivendor/MainActivity.java
@@ -0,0 +1,6 @@
+package com.annadatanew.user;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,24 @@
+final String appName = 'annadata';
+
+final String packageName = 'com.annadatanew.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.annadatanew.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.annadatanew.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+// final String baseUrl = 'https://buransmart.in/app/v1/api/';
+//  final String baseUrl = 'https://developmentalphawizz.com/new_annadata_sukhi_bhav/app/v1/api/';
+ final String baseUrl = 'https://developmentalphawizz.com/new_annadata_sukhi_bhav/app/v1/api/';
+// final String baseUrl = 'https://developmentalphawizz.com/farmbuz/app/v1/api/';
+
+ final String imageUrl = 'https://developmentalphawizz.com/new_annadata_sukhi_bhav/';
+// final String imageUrl = 'https://developmentalphawizz.com/farmbuz/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
